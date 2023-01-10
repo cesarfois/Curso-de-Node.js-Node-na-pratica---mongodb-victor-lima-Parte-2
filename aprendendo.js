4685cda83ddd37c631b5a861c2df254545361693
unknown <fois2010@gmail.com>
2023-01-10 11:35:48 -0300

    adicionando usuario no mongodb - fim do modulo

--- aprendendo.js
+++ aprendendo.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose")
+
+
+mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", true);
+
 mongoose.connect('mongodb+srv://fois2010:Siexpre$$@cluster0.9e3ejn1.mongodb.net/?retryWrites=true&w=majority').then(() => {
     console.log('MongoDB conectado com sucesso')
 })
@@ -7,3 +11,54 @@ mongoose.connect('mongodb+srv://fois2010:Siexpre$$@cluster0.9e3ejn1.mongodb.net/
     console.log('houve um erro ao se conectar ao mongoDB:' + error )
 
 })
+
+
+// Models
+
+const usersShema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    },
+})
+
+// Definindo a collection
+mongoose.model('users', usersShema)
+
+
+// adicionado um registro
+
+const novousuario = mongoose.model('users')
+
+new novousuario({
+    nome: "javier",
+    sobrenome: "fois",
+    email: "emaifl@gmail.com",
+    idade: 44,
+    pais: "Uruguay"
+}).save()
+.then(() => {
+    console.log('usuario cadastrado com sucesso')
+})
+.catch((e) =>{
+    console.log('Erro ao cadastrar um novo usuario', e) 
+})
+
+
+
+
