6cefbe2b121f74405a133a8e833976c3b9686534
unknown <fois2010@gmail.com>
2023-01-10 11:53:34 -0300

    Começando projeto Blog

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const handlebars= require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const mongoose = require('mongoose')
+
+
+
+// Configurações
+ // Body Parser
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use(bodyParser.json())
+
+    // Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+
+
+// Rotas
+
+const port = 8081
+
+ app.listen(port, () => {
+    console.log('Servidor Rodando!')
+ })
