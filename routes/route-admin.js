dd2a6364a54685fdf46fd3066f7189c6496e603d
unknown <fois2010@gmail.com>
2023-01-11 17:21:34 -0300

    Adicionando formulario de categorias

--- routes/route-admin.js
+++ routes/route-admin.js
@@ -15,8 +15,14 @@ router.get ('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.send('Pagina de categorias')
+    res.render("admin/categorias")
 })
+
+
+router.get('/categorias/add', (req, res) => {
+    res.render('admin/addcategorias')
+})
+
 
 
 
