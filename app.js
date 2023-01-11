bff398b197ff6f5a4afa4c56379f63c159ecde1a
unknown <fois2010@gmail.com>
2023-01-11 17:42:39 -0300

    Conectando a MongoDB e adicionando o Model Categoria Aula36

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 // arquivo necessario para falar para o express onde esta as rotas
 const admin = require('./routes/route-admin');
 const path = require('path')
+const mongoose = require('mongoose')
 
 // Configurações
  
@@ -14,6 +15,14 @@ const path = require('path')
     // Handlebars
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
     app.set('view engine', 'handlebars');
+
+    // Moongoose
+      mongoose.Promise = global.Promise;
+      mongoose.connect('mongodb+srv://fois2010:Siexpre$$@cluster0.9e3ejn1.mongodb.net/?retryWrites=true&w=majority').then(() => {
+         console.log('Conectado a MongoDB com sucesso')
+      }).catch((error) => {
+         console.log('Erro ao se conectar com mongoDB' + error)
+      })
 
     // Public
     app.use(express.static(path.join(__dirname,'/public')))
