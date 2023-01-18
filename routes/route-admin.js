fce077b1acb625953fb599018a7715a9640e56ae
unknown <fois2010@gmail.com>
2023-01-18 17:29:50 -0300

    Adicionando formulario de Postagem - Aula 45

--- routes/route-admin.js
+++ routes/route-admin.js
@@ -106,7 +106,21 @@ router.post("/categorias/deletar", (req,res) => {
         res.redirect("/admin/categorias")
     })
 })
+
+
+router.get("/postagens", (req, res) => {
+    res.render('admin/postagens')
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render('admin/addpostagens', {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error_msg', "Houve um erro ao carregar o formulario")
+        res.redirect('/admin')
+    })
     
+})
 
 
     
